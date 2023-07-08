d3163a6b14ae5446fa79997ac90f0aa91ba05125
git <luis.f.blanco@gmail.com>
2023-07-08 11:08:47 -0500

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <>
+            <header>[Header]</header>
+            <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+            <footer>[Footer]</footer>
+        </>
+    );
+}
